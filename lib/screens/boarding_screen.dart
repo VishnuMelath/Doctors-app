83de64d8e0c02029bd6576baf9642749c85f98ad
vishnu <vishnumeloth@gmail.com>
2024-12-06 21:53:10 +0530

    boarding screen completed

--- lib/screens/boarding_screen.dart
+++ lib/screens/boarding_screen.dart
@@ -1,10 +1,151 @@
+import 'dart:ui';
+
+import 'package:doctors/theme/font_style.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class BoardingScreen extends StatelessWidget {
   const BoardingScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+      statusBarColor: Colors.white,
+      statusBarBrightness: Brightness.dark,
+      statusBarIconBrightness: Brightness.dark,
+    ));
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Colors.white,
+        body: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Stack(
+                children: [
+                  Positioned(
+                    bottom: 7,
+                    child: Container(
+                      width: MediaQuery.sizeOf(context).width - 40,
+                      height: 200,
+                      decoration: BoxDecoration(
+                          color: Colors.blue[600],
+                          borderRadius: BorderRadius.circular(25)),
+                    ),
+                  ),
+                  Positioned(
+                      bottom: 185,
+                      child: Image.asset(
+                        'assets/injection.png',
+                        height: 40,
+                        filterQuality: FilterQuality.none,
+                      )),
+                  Positioned(
+                      bottom: 185,
+                      right: 0,
+                      child: Image.asset(
+                        'assets/medical.png',
+                        height: 40,
+                        filterQuality: FilterQuality.none,
+                      )),
+                  Positioned(
+                      bottom: 0,
+                      child: Transform.rotate(
+                        angle: -0.3,
+                        child: Image.asset(
+                          'assets/smiley.png',
+                          height: 40,
+                          filterQuality: FilterQuality.none,
+                        ),
+                      )),
+                  Align(
+                      alignment: Alignment.bottomCenter,
+                      child: Image.asset('assets/doctor.png'))
+                ],
+              ),
+              const Spacer(
+                flex: 1,
+              ),
+              ShaderMask(
+                shaderCallback: (bounds) {
+                  return LinearGradient(
+                    colors: [Colors.blue[600]!, Colors.blue[50]!],
+                    begin: Alignment.topLeft,
+                    end: Alignment.bottomRight,
+                  ).createShader(bounds);
+                },
+                blendMode: BlendMode.srcIn,
+                child: FittedBox(
+                  fit: BoxFit.scaleDown,
+                  child: Text(
+                    'Hey Doctors.!',
+                    style: GoogleFonts.montserrat(
+                        fontSize: 42, fontWeight: FontWeight.bold),
+                  ),
+                ),
+              ),
+              Text(
+                'Don\'t Fear this is Right Platforms for you',
+                style: regularSizeBlackFont,
+              ),
+              Wrap(
+                alignment: WrapAlignment.start,
+                children: [
+                  ShaderMask(
+                    shaderCallback: (bounds) {
+                      return LinearGradient(
+                        colors: [Colors.blue[600]!, Colors.blue[50]!],
+                        begin: Alignment.topLeft,
+                        end: Alignment.bottomRight,
+                      ).createShader(bounds);
+                    },
+                    blendMode: BlendMode.srcIn,
+                    child: FittedBox(
+                      fit: BoxFit.scaleDown,
+                      child: Text(
+                        '10,000+',
+                        style: regularSizeBlackFont,
+                      ),
+                    ),
+                  ),
+                  Text(
+                    ' Jobs Available for your ',
+                    style: regularSizeBlackFont,
+                  ),
+                ],
+              ),
+              const Text('Interested.'),
+              const Spacer(
+                flex: 3,
+              ),
+              Align(
+                alignment: Alignment.centerRight,
+                child: InkWell(
+                  onTap: () {},
+                  child: Container(
+                    padding: const EdgeInsets.all(14),
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(100),
+                        color: Colors.blue[600]),
+                    child: Text(
+                      'Explore all jobs',
+                      style: montserrat.copyWith(
+                          color: Colors.white,
+                          fontSize: 15,
+                          fontWeight: FontWeight.w700),
+                    ),
+                  ),
+                ),
+              ),
+              const Spacer(
+                flex: 1,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -2,6 +2,7 @@ import 'dart:developer';
 
 import 'package:doctors/screens/boarding_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class SplashScreen extends StatefulWidget {
@@ -19,6 +20,13 @@ class _SplashScreenState extends State<SplashScreen>
   late Animation<double> _opacityAnimation;
   @override
   void initState() {
+    SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+      statusBarColor:
+          Color.fromARGB(255, 27, 111, 207), // Status bar background color
+      statusBarBrightness: Brightness.light, // For iOS: text/icons color
+      statusBarIconBrightness:
+          Brightness.light, // For Android: text/icons color
+    ));
     _controller = AnimationController(
       duration: const Duration(seconds: 2),
       vsync: this,
@@ -63,80 +71,85 @@ class _SplashScreenState extends State<SplashScreen>
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: const Color.fromARGB(255, 27, 111, 207),
-      body: Stack(
-        children: [
-          Positioned(
-              bottom: 0,
-              child: SizedBox(
-                child: Image.asset(
-                  'assets/splash_image.png',
-                  fit: BoxFit.fill,
-                  height: MediaQuery.sizeOf(context).height * 0.7,
-                  width: MediaQuery.sizeOf(context).width,
-                ),
-              )),
-          Container(
-            height: MediaQuery.sizeOf(context).height,
-            width: MediaQuery.sizeOf(context).width,
-            decoration: BoxDecoration(
-                gradient: LinearGradient(
-                    begin: Alignment.topCenter,
-                    end: Alignment.bottomCenter,
-                    stops: const [
-                  0.35,
-                  1
-                ],
-                    colors: [
-                  const Color.fromARGB(228, 21, 101, 192)!,
-                  const Color.fromARGB(0, 30, 52, 151)
-                ])),
-            child: Center(
-              child: SizedBox(
-                height: 80,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    SlideTransition(
-                      position: _docAnimation,
-                      child: Container(
-                          padding: const EdgeInsets.symmetric(
-                              horizontal: 8, vertical: 1),
-                          decoration: BoxDecoration(
-                              color: Colors.white,
-                              borderRadius: BorderRadius.circular(100)),
-                          child: Text(
-                            'DOC',
-                            style: GoogleFonts.cormorantSc(
-                                fontWeight: FontWeight.bold,
-                                fontSize: 25,
-                                color: Colors.blueAccent),
-                          )),
+    return ColoredBox(
+      color: const Color.fromARGB(255, 27, 111, 207),
+      child: SafeArea(
+        child: Scaffold(
+          backgroundColor: const Color.fromARGB(255, 27, 111, 207),
+          body: Stack(
+            children: [
+              Positioned(
+                  bottom: 0,
+                  child: SizedBox(
+                    child: Image.asset(
+                      'assets/splash_image.png',
+                      fit: BoxFit.fill,
+                      height: MediaQuery.sizeOf(context).height * 0.7,
+                      width: MediaQuery.sizeOf(context).width,
                     ),
-                    SlideTransition(
-                      position: _findAnimation,
-                      child: Padding(
-                        padding: const EdgeInsets.only(left: 5.0),
-                        child: FadeTransition(
-                          opacity: _opacityAnimation,
-                          child: Text(
-                            'FIND',
-                            //   style: TextStyle(fontSize: 25, color: Colors.white),
-                            style: GoogleFonts.cormorantSc(
-                                fontWeight: FontWeight.bold,
-                                fontSize: 25,
-                                color: Colors.white),
-                          ),
+                  )),
+              Container(
+                height: MediaQuery.sizeOf(context).height,
+                width: MediaQuery.sizeOf(context).width,
+                decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                        begin: Alignment.topCenter,
+                        end: Alignment.bottomCenter,
+                        stops: const [
+                      0.35,
+                      1
+                    ],
+                        colors: [
+                      const Color.fromARGB(228, 21, 101, 192)!,
+                      const Color.fromARGB(0, 30, 52, 151)
+                    ])),
+                child: Center(
+                  child: SizedBox(
+                    height: 80,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        SlideTransition(
+                          position: _docAnimation,
+                          child: Container(
+                              padding: const EdgeInsets.symmetric(
+                                  horizontal: 8, vertical: 1),
+                              decoration: BoxDecoration(
+                                  color: Colors.white,
+                                  borderRadius: BorderRadius.circular(100)),
+                              child: Text(
+                                'DOC',
+                                style: GoogleFonts.cormorantSc(
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 25,
+                                    color: Colors.blueAccent),
+                              )),
                         ),
-                      ),
-                    )
-                  ],
+                        SlideTransition(
+                          position: _findAnimation,
+                          child: Padding(
+                            padding: const EdgeInsets.only(left: 5.0),
+                            child: FadeTransition(
+                              opacity: _opacityAnimation,
+                              child: Text(
+                                'FIND',
+                                //   style: TextStyle(fontSize: 25, color: Colors.white),
+                                style: GoogleFonts.cormorantSc(
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 25,
+                                    color: Colors.white),
+                              ),
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
                 ),
               ),
-            ),
+            ],
           ),
-        ],
+        ),
       ),
     );
   }
